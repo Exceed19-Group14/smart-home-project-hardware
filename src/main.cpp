31676662ba7b6370b7e206fcbd96d2ac8ce2ecdf
teenteennnii <teennopwijit@gmail.com>
2023-02-08 13:37:52 +0700

    light on off

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,49 @@ Bounce debouncer = Bounce();
 const int ledPin =  2;      // the number of the LED pin
 const int threshold =  40;      // the number of the LED pin
 int ledState = LOW;             // ledState used to set the LED
-int l6_val = 0;
+int count = 0;
 
 void click () {
-  debouncer.update();
-    if ( debouncer.rose() ) { 
-        ledcWrite(0, 255);
-    }
+  
+  count++;
   
   // Serial.println("Click");
+}
+
+void green(){
+  if (touchRead(T6) < 25 && count == 0){
+    ledcWrite(0, 255);
+  } else if (touchRead(T6) < 25 && count == 1){
+    ledcWrite(0, 0);
+  } else if (touchRead(T6) > 40 && count == 0){
+    count = 1;
+  } else if (touchRead(T6) > 40 && count == 1){
+    count = 0;
+  }
+}
+
+void yellow(){
+  if (touchRead(T9) < 25 && count == 0){
+    ledcWrite(1, 255);
+  } else if (touchRead(T9) < 25 && count == 1){
+    ledcWrite(1, 0);
+  } else if (touchRead(T9) > 40 && count == 0){
+    count = 1;
+  } else if (touchRead(T9) > 40 && count == 1){
+    count = 0;
+  }
+}
+
+void red(){
+  if (touchRead(T5) < 25 && count == 0){
+    ledcWrite(2, 255);
+  } else if (touchRead(T5) < 25 && count == 1){
+    ledcWrite(2, 0);
+  } else if (touchRead(T5) > 40 && count == 0){
+    count = 1;
+  } else if (touchRead(T5) > 40 && count == 1){
+    count = 0;
+  }
 }
 
 void setup() {
@@ -42,11 +76,12 @@ void setup() {
   ledcAttachPin(YELLOW, 1);
   ledcAttachPin(RED, 2);
 
-  debouncer.attach(T6, INPUT_PULLUP);
-  debouncer.interval(25); 
-  touchAttachInterrupt(T6 , click , threshold );
+  touchAttachInterrupt(T6 , green , threshold );
+  touchAttachInterrupt(T9 , yellow , threshold );
+  touchAttachInterrupt(T5 , red , threshold );
 
 }
 void loop() {    
-  
+  delay(500);
+  Serial.printf("%d %d %d\n", touchRead(T6), touchRead(T9), touchRead(T5));
 }
