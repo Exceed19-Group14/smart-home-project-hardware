3e91651d2413c9fe7d2e580545bc4823a2421920
teenteennnii <teennopwijit@gmail.com>
2023-02-08 13:59:42 +0700

    update conect wifi

--- src/main.cpp
+++ src/main.cpp
@@ -20,49 +20,94 @@ Bounce debouncer = Bounce();
 const int ledPin =  2;      // the number of the LED pin
 const int threshold =  40;      // the number of the LED pin
 int ledState = LOW;             // ledState used to set the LED
-int count = 0;
+int count = 0, check_ldr = 0, ldr_auto = 0;
 
-void click () {
-  
-  count++;
-  
-  // Serial.println("Click");
+void Connect_Wifi() {
+  const char *ssid = "Illya";
+  const char *password = "teen12345";
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("OK! IP=");
+  Serial.println(WiFi.localIP());
 }
 
 void green(){
-  if (touchRead(T6) < 25 && count == 0){
-    ledcWrite(0, 255);
-  } else if (touchRead(T6) < 25 && count == 1){
-    ledcWrite(0, 0);
-  } else if (touchRead(T6) > 40 && count == 0){
-    count = 1;
-  } else if (touchRead(T6) > 40 && count == 1){
-    count = 0;
+  if (ldr_auto == 0){
+    if (touchRead(T6) < 25 && count == 0){
+      ledcWrite(0, 255);
+    } else if (touchRead(T6) < 25 && count == 1){
+      ledcWrite(0, 0);
+    } else if (touchRead(T6) > 40 && count == 0){
+      count = 1;
+    } else if (touchRead(T6) > 40 && count == 1){
+      count = 0;
+    }
   }
 }
 
 void yellow(){
-  if (touchRead(T9) < 25 && count == 0){
-    ledcWrite(1, 255);
-  } else if (touchRead(T9) < 25 && count == 1){
-    ledcWrite(1, 0);
-  } else if (touchRead(T9) > 40 && count == 0){
-    count = 1;
-  } else if (touchRead(T9) > 40 && count == 1){
-    count = 0;
+  if (ldr_auto == 0){
+    if (touchRead(T9) < 25 && count == 0){
+      ledcWrite(1, 255);
+    } else if (touchRead(T9) < 25 && count == 1){
+      ledcWrite(1, 0);
+    } else if (touchRead(T9) > 40 && count == 0){
+      count = 1;
+    } else if (touchRead(T9) > 40 && count == 1){
+      count = 0;
+    }
   }
 }
 
 void red(){
-  if (touchRead(T5) < 25 && count == 0){
-    ledcWrite(2, 255);
-  } else if (touchRead(T5) < 25 && count == 1){
-    ledcWrite(2, 0);
-  } else if (touchRead(T5) > 40 && count == 0){
-    count = 1;
-  } else if (touchRead(T5) > 40 && count == 1){
-    count = 0;
+  if (ldr_auto == 0){
+    if (touchRead(T5) < 25 && count == 0){
+      ledcWrite(2, 255);
+    } else if (touchRead(T5) < 25 && count == 1){
+      ledcWrite(2, 0);
+    } else if (touchRead(T5) > 40 && count == 0){
+      count = 1;
+    } else if (touchRead(T5) > 40 && count == 1){
+      count = 0;
+    }
   }
+}
+
+void LDR_control_auto_green(){
+  check_ldr = (map(analogRead(LDR),2000,4095,0,255));
+      if (check_ldr < 150) {
+        ledcWrite(0, 255);
+      } else {
+        ledcWrite(0, 0);
+      }
+}
+
+void LDR_control_auto_yellow(){
+  check_ldr = (map(analogRead(LDR),2000,4095,0,255));
+      if (check_ldr < 150) {
+        ledcWrite(1, 255);
+      } else {
+        ledcWrite(1, 0);
+      }
+}
+
+void LDR_control_auto_red(){
+  check_ldr = (map(analogRead(LDR),2000,4095,0,255));
+      if (check_ldr < 150) {
+        ledcWrite(1, 255);
+      } else {
+        ledcWrite(1, 0);
+      }
+}
+
+void LDR_control_auto(){
+  LDR_control_auto_green();
+  LDR_control_auto_yellow();
+  LDR_control_auto_red();
 }
 
 void setup() {
@@ -76,9 +121,13 @@ void setup() {
   ledcAttachPin(YELLOW, 1);
   ledcAttachPin(RED, 2);
 
+  Connect_Wifi();
+
   touchAttachInterrupt(T6 , green , threshold );
   touchAttachInterrupt(T9 , yellow , threshold );
   touchAttachInterrupt(T5 , red , threshold );
+
+  LDR_control_auto();
 
 }
 void loop() {    
